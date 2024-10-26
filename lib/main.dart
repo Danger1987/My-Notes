ad77544a303430a6a3b4722359e08cdd41f8e9b1
Danger1987 <sakibmohd072@gmail.com>
2024-10-26 16:39:39 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
         title: const Text('Register'),
